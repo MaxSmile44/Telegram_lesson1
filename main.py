4be7cee9ead0743af3e6a5fc9274f3d6282a4bd7
MaxSmile44 <afmaxim89@gmail.com>
2025-05-22 17:48:58 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import argparse
+import os
+
+import requests
+import telegram
+
+from dotenv import load_dotenv
+from time import sleep
+
+
+def send_tg_message(tg_token, chat_id, text):
+    bot = telegram.Bot(token=tg_token)
+    bot.send_message(chat_id=chat_id, text=text)
+
+
+def looking_for_completed_works(dev_token, tg_token, chat_id):
+    headers = {'Authorization': f'Token {dev_token}'}
+    while True:
+        try:
+            response = requests.get('https://dvmn.org/api/long_polling/', headers=headers)
+            response.raise_for_status()
+            response_json = response.json()
+            if response_json['status'] == 'found':
+                if response_json['new_attempts'][0]['is_negative']:
+                    send_tg_message(
+                        tg_token, chat_id,
+                        f"У вас проверили работу \"{response_json['new_attempts'][0]['lesson_title']}\"\n\n"
+                        f"К сожалению, в работе нашлись ошибки.\n"
+                        f"{response_json['new_attempts'][0]['lesson_url']}"
+                    )
+                else:
+                    send_tg_message(
+                        tg_token, chat_id,
+                        f"У вас проверили работу \"{response_json['new_attempts'][0]['lesson_title']}\"\n\n"
+                        f"Преподавателю всё понравилось, можно приступать к следующему уроку!\n"
+                        f"{response_json['new_attempts'][0]['lesson_url']}"
+                    )
+        except requests.ReadTimeout:
+            print('Таймаут вышел')
+        except requests.exceptions.ConnectionError:
+            print('Отсутствует подключение к интернету')
+            sleep(10)
+        except requests.exceptions.HTTPError as error:
+            print(error)
+            break
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Укажите свой chat_id телеграма')
+    parser.add_argument('-c', '--chat_id', type=int, help='Ваш chat_id')
+    args = parser.parse_args()
+    chat_id = args.chat_id
+    try:
+        load_dotenv()
+        dev_token = os.environ['DEVMAN_TOKEN']
+        tg_token = os.environ['TG_TOKEN']
+    except KeyError as error:
+        print(f'KeyError: {error}')
+        raise SystemExit
+    looking_for_completed_works(dev_token, tg_token, chat_id)
+
+
+if __name__ == '__main__':
+    main()
